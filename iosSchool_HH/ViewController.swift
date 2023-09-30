ac59590c63cc170357faa631c801dd31f3c3638b
Vaganova Ann <vaganova.ann00@gmail.com>
2023-09-30 18:18:30 +0700

    Add project

---
+++ iosSchool_HH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosSchool_HH
+//
+//  Created by Анна on 30.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
